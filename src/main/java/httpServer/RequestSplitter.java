de4e97d7f2793283c780a78b22e93808af83fb50
Elle Hallal <daniellehallal@gmail.com>
2019-07-11 15:54:40 +0100

    Change RequestSplitter to initialise with request

--- src/main/java/httpServer/RequestSplitter.java
+++ src/main/java/httpServer/RequestSplitter.java
@@ -1,18 +1,23 @@
 package httpServer;
 
 public class RequestSplitter {
+    public RequestSplitter(String request) {
+        this.request = request;
+    }
 
-    static String getRequestMethod(String request) {
-        var splitRequest = splitRequestLine(request);
+    private final String request;
+
+    String getRequestMethod() {
+        var splitRequest = splitRequestLine();
         return splitRequest[0];
     }
 
-    static String getRequestPath(String request) {
-        var splitRequest = splitRequestLine(request);
+    String getRequestPath() {
+        var splitRequest = splitRequestLine();
         return splitRequest[1];
     }
 
-    private static String[] splitRequestLine(String request) {
+    private String[] splitRequestLine() {
         return request.split(" ");
     }
 }
