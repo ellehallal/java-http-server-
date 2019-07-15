3ffc8d25ff47ada9253699dca7deb76231e4ac56
Elle H <itsellej@gmail.com>
2019-07-15 16:21:25 +0100

    Refactor: inline expected output

--- src/test/java/httpserver/http/route/RouteHandlerTest.java
+++ src/test/java/httpserver/http/route/RouteHandlerTest.java
@@ -7,41 +7,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RouteHandlerTest {
     @Test
     void returns200StatusWithNoHeadersAndEmptyBodyWhenRequestPathIsSimpleGet() {
-        var expectedOutput = "HTTP/1.1 200 OK";
         var request = "GET /simple_get HTTP/1.1";
         var routeHandler = new RouteHandler(request);
         var response = routeHandler.getResponse();
 
-        assertEquals(expectedOutput, response);
+        assertEquals("HTTP/1.1 200 OK", response);
     }
 
     @Test
     void returns404StatusWhenPathDoesNotExist() {
-       var expectedOutput = "HTTP/1.1 404 NOT FOUND";
         var request = "GET /some_path HTTP/1.1";
         var routeHandler = new RouteHandler(request);
         var response = routeHandler.getResponse();
 
-        assertEquals(expectedOutput, response);
+        assertEquals("HTTP/1.1 404 NOT FOUND", response);
     }
 
     @Test
     void returns200StatusWithHeadersAndEmptyBodyWhenRequestMethodIsOptionsAndPathIsMethodOptions() {
-        var expectedOutput = "HTTP/1.1 200 OK\nAllow: GET, HEAD, OPTIONS";
         var request = "OPTIONS /method_options HTTP/1.1";
         var routeHandler = new RouteHandler(request);
         var response = routeHandler.getResponse();
 
-        assertEquals(expectedOutput, response);
+        assertEquals("HTTP/1.1 200 OK\nAllow: GET, HEAD, OPTIONS", response);
     }
 
     @Test
     void returns200StatusWithHeadersAndEmptyBodyWhenRequestMethodIsOptionsAndPathIsMethodOptions2() {
-        var expectedOutput = "HTTP/1.1 200 OK\nAllow: GET, HEAD, OPTIONS, PUT, POST";
         var request = "OPTIONS /method_options2 HTTP/1.1";
         var routeHandler = new RouteHandler(request);
         var response = routeHandler.getResponse();
 
-        assertEquals(expectedOutput, response);
+        assertEquals("HTTP/1.1 200 OK\nAllow: GET, HEAD, OPTIONS, PUT, POST", response);
     }
 }
