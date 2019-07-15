ea8c0c4e8baae5c805e8adda8e6508d3bf6654d3
Elle H <itsellej@gmail.com>
2019-07-15 17:38:05 +0100

    RouteHandler: remove notFound()

--- src/main/java/httpserver/http/route/RouteHandler.java
+++ src/main/java/httpserver/http/route/RouteHandler.java
@@ -1,11 +1,9 @@
 package httpserver.http.route;
 
 
-import httpserver.http.Protocol;
 import httpserver.http.RequestMethod;
-import httpserver.http.StatusCode;
 import httpserver.http.request.RequestSplitter;
-import httpserver.http.response.Response;
+import httpserver.http.response.ResponseBuilder;
 import httpserver.http.route.requestmethod.GetMethodHandler;
 import httpserver.http.route.requestmethod.OptionsMethodHandler;
 
@@ -29,13 +27,6 @@ public class RouteHandler {
         } else if (requestMethod.equals(RequestMethod.OPTIONS.toString())) {
             return new OptionsMethodHandler().getResponse(requestPath);
         }
-        return notFound();
-    }
-
-    private String notFound() {
-        return new Response()
-                .setProtocol(Protocol.HTTP_1_1)
-                .setStatusCode(StatusCode.NOT_FOUND)
-                .build();
+        return ResponseBuilder.buildNotFoundResponse();
     }
 }
