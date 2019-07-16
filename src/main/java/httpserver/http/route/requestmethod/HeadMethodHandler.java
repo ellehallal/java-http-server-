9a75d6849a1b3331d76983ea9ddf26324118eace
Elle Hallal <daniellehallal@gmail.com>
2019-07-16 13:22:55 +0100

    Create HeadMethodHandler

---
+++ src/main/java/httpserver/http/route/requestmethod/HeadMethodHandler.java
@@ -0,0 +1,23 @@
+package httpserver.http.route.requestmethod;
+
+import httpserver.http.StatusCode;
+import httpserver.http.request.Request;
+import httpserver.http.response.ResponseFactory;
+
+public class HeadMethodHandler {
+
+    public String getResponse(Request request) {
+        var requestPath = request.getRequestPath();
+        switch (requestPath) {
+            case "/simple_get":
+                return buildResponseString(StatusCode.OK, null, null);
+            default:
+                return buildResponseString(StatusCode.NOT_FOUND, null, null);
+        }
+    }
+
+    private String buildResponseString(StatusCode statusCode, String headerName, String headerValue) {
+        var response = ResponseFactory.build(statusCode, headerName, headerValue);
+        return response.toString();
+    }
+}
