4a70bc557aa6c53bb0fc013f0ec8e345e95a193e
Elle Hallal <daniellehallal@gmail.com>
2019-07-16 09:48:45 +0100

    Create Request class

---
+++ src/main/java/httpserver/http/request/Request.java
@@ -0,0 +1,22 @@
+package httpserver.http.request;
+
+public class Request {
+    private String requestMethod;
+    private String requestPath;
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public void setRequestMethod(String requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+    public String getRequestPath() {
+        return requestPath;
+    }
+
+    public void setRequestPath(String requestPath) {
+        this.requestPath = requestPath;
+    }
+}
