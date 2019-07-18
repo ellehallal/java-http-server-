ad090d1e1e2afad4c25738eb685d781bee28c1ac
Elle H <itsellej@gmail.com>
2019-07-18 12:42:12 +0100

    StatusCode - add 301 moved permanently

--- src/main/java/httpserver/http/StatusCode.java
+++ src/main/java/httpserver/http/StatusCode.java
@@ -2,6 +2,7 @@ package httpserver.http;
 
 public enum StatusCode {
     OK("200 OK"),
+    MOVED_PERMANENTLY("301 MOVED PERMANENTLY"),
     NOT_FOUND("404 NOT FOUND"),
     METHOD_NOT_ALLOWED("405 METHOD NOT ALLOWED");
 
