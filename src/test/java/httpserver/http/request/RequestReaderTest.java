0e41bb3f817079e1aaba2098fcb9be7ecaa1a164
Elle H <itsellej@gmail.com>
2019-07-20 13:25:31 +0100

    RequestReader: reads body bytes of request

--- src/test/java/httpserver/http/request/RequestReaderTest.java
+++ src/test/java/httpserver/http/request/RequestReaderTest.java
@@ -10,12 +10,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RequestReaderTest {
 
     @Test
-    void returnsInput() {
+    void returnsInputWithRequestLine() {
         var stringReader = new StringReader("GET / HTTP/1.1");
         var input = new BufferedReader(stringReader);
 
-        var request = RequestReader.read(input);
+        var request = new RequestReader(input).read();
 
-        assertEquals("GET / HTTP/1.1", request);
+        assertEquals("GET / HTTP/1.1\r\n", request);
+    }
+
+    @Test
+    void returnsInputWithRequestLineAndHeaders() {
+        var stringReader = new StringReader("GET / HTTP/1.1\r\nHost: localhost\r\n\r\n");
+        var input = new BufferedReader(stringReader);
+
+        var request = new RequestReader(input).read();
+
+        assertEquals("GET / HTTP/1.1\r\nHost: localhost\r\n", request);
+    }
+
+    @Test
+    void returnsInputWithRequestLineHeadersANDBODY() {
+        var stringReader = new StringReader
+                ("POST / HTTP/1.1\r\nHost: localhost\r\n\r\nHello");
+        var input = new BufferedReader(stringReader);
+
+        var request = new RequestReader(input).read();
+
+        assertEquals("POST / HTTP/1.1\r\nHost: localhost\r\n\r\nHello", request);
     }
 }
