61c08025ef65bf38c0b041d6a69e6b00852f272b
Elle H <itsellej@gmail.com>
2019-08-01 11:54:21 +0100

    Routes - adds a Route to an existing path. If Route exists under path, Route is not added

--- src/main/java/httpserver/route/Routes.java
+++ src/main/java/httpserver/route/Routes.java
@@ -1,27 +1,33 @@
 package httpserver.route;
 
+import httpserver.RequestMethod;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Routes {
-    private final HashMap<String, ArrayList<Route>> routes = new HashMap<>();
+    private final HashMap<String, ArrayList<Route>> pathsAndRoutes = new HashMap<>();
 
     public void addRoute(Route route) {
-        if(doesRouteExist(route)) {
+        if(doesPathExist(route)) {
             addToExistingPath(route);
         } else {
             createNewPath(route);
         }
     }
 
-    private boolean doesRouteExist(Route route) {
+    private boolean doesPathExist(Route route) {
         var path = route.getPath();
-        return routes.containsKey(path);
+        return pathsAndRoutes.containsKey(path);
     }
 
     private void addToExistingPath(Route route) {
         var path = route.getPath();
-        routes.get(path).add(route);
+        var requestMethod = route.getRequestMethod();
+
+        if(!doesRouteExist(path, requestMethod)) {
+            getRoutes(path).add(route);
+        }
     }
 
     private void createNewPath(Route route) {
@@ -29,10 +35,36 @@ public class Routes {
         var routeList = new ArrayList<Route>();
 
         routeList.add(route);
-        routes.put(path, routeList);
+        pathsAndRoutes.put(path, routeList);
     }
 
-    public HashMap<String, ArrayList<Route>> getRoutes() {
-        return routes;
+    public HashMap<String, ArrayList<Route>> getPathsAndRoutes() {
+        return pathsAndRoutes;
+    }
+
+    public ArrayList<Route> getRoutes(String path) {
+        return pathsAndRoutes.get(path);
+    }
+
+    public Route getASingleRoute(String path, RequestMethod requestMethod) {
+        var routes = getRoutes(path);
+
+        for(Route route: routes) {
+           if(route.getRequestMethod() == requestMethod) {
+               return route;
+           }
+        }
+        return null;
+    }
+
+    private boolean doesRouteExist(String path, RequestMethod requestMethod) {
+        var routes = getRoutes(path);
+
+        for(Route route: routes) {
+            if(route.getRequestMethod() == requestMethod) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/httpserver/route/RoutesTest.java
+++ src/test/java/httpserver/route/RoutesTest.java
@@ -7,18 +7,59 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RoutesTest {
     @Test
-    void addsARouteObject() {
+    void addsARouteObjectToANewPath() {
 
         var methodHandler = new FakeMethodHandler();
         var routes = new Routes();
         var route = RouteBuilder.build(RequestMethod.GET, "/simple_get", methodHandler);
 
         routes.addRoute(route);
-        var allRoutes = routes.getRoutes();
+        var allRoutes = routes.getPathsAndRoutes();
+        var getSimpleGetRoute = routes.getASingleRoute("/simple_get", RequestMethod.GET);
 
         assertEquals(1, allRoutes.size());
-        assertEquals(RequestMethod.GET, allRoutes.get("/simple_get").get(0).getRequestMethod());
-        assertEquals("/simple_get", allRoutes.get("/simple_get").get(0).getPath());
-        assertEquals(methodHandler, allRoutes.get("/simple_get").get(0).getMethodHandler());
+        assertEquals(RequestMethod.GET, getSimpleGetRoute.getRequestMethod());
+        assertEquals("/simple_get", getSimpleGetRoute.getPath());
+        assertEquals(methodHandler, getSimpleGetRoute.getMethodHandler());
+    }
+
+    @Test
+    void addsARouteObjectToAnExistingPath() {
+
+        var methodHandler = new FakeMethodHandler();
+        var routes = new Routes();
+        var getSimpleGetRoute = RouteBuilder.build(RequestMethod.GET, "/simple_get", methodHandler);
+        var postSimpleGetRoute = RouteBuilder.build(RequestMethod.POST, "/simple_get", methodHandler);
+
+        routes.addRoute(getSimpleGetRoute);
+        routes.addRoute(postSimpleGetRoute);
+        var allRoutes = routes.getPathsAndRoutes();
+        var getRoute = routes.getASingleRoute("/simple_get", RequestMethod.GET);
+        var postRoute = routes.getASingleRoute("/simple_get", RequestMethod.POST);
+
+        assertEquals(1, allRoutes.size());
+        assertEquals(2, allRoutes.get("/simple_get").size());
+        assertEquals(RequestMethod.GET, getRoute.getRequestMethod());
+        assertEquals("/simple_get", getRoute.getPath());
+        assertEquals(methodHandler, getRoute.getMethodHandler());
+        assertEquals(RequestMethod.POST, postRoute.getRequestMethod());
+        assertEquals("/simple_get", postRoute.getPath());
+        assertEquals(methodHandler, postRoute.getMethodHandler());
+    }
+
+    @Test
+    void ifRouteExistsItIsNotAddedToRoutes() {
+
+        var methodHandler = new FakeMethodHandler();
+        var routes = new Routes();
+        var getSimpleGetRoute1 = RouteBuilder.build(RequestMethod.GET, "/simple_get", methodHandler);
+        var getSimpleGetRoute2 = RouteBuilder.build(RequestMethod.GET, "/simple_get", methodHandler);
+
+        routes.addRoute(getSimpleGetRoute1);
+        routes.addRoute(getSimpleGetRoute2);
+        var allRoutes = routes.getPathsAndRoutes();
+
+        assertEquals(1, allRoutes.size());
+        assertEquals(1, allRoutes.get("/simple_get").size());
     }
 }
